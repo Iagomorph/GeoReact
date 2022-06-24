b4ccefda878080893bdd7a8893f66b9b8ad1d1b9
Philémon Wild <philemon.wild@gmail.com>
2022-06-24 04:35:10 +0200

    invalid hook call ?

--- src/components/DepartementList.jsx
+++ src/components/DepartementList.jsx
@@ -1,20 +1,38 @@
-import { useEffect, useState, useRef } from "react";
+import React,{ useEffect, useState } from "react";
 
 
 function DepartementList(){
-    const [data, setData] = useState(null);
-    // const [loading, setLoading] = useState(true);
-    // const [error, setError] = useState(null);
     
-
-
+    const [dpData, setDpData] = useState();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    
+    useEffect(() =>{
+        fetch("https://geo.api.gouv.fr/departements")
+        .then((response)=> response.json())
+        .then((jsonData)=>{
+            setDpData(jsonData);
+            setError(null);
+        })
+        .catch((err)=> {
+            setError(err.message);
+            setDpData(null);
+        })
+        .finally(() => {
+            setLoading(false);
+        });
+    },[]);
 
 
 return(
     <>
     <h2>Filtrer par départements</h2>
+    {loading && <div>A moment please...</div>}
+    {error && (
+        <div>{`une erreur est survenue en récupérant les donées - ${error}`}</div>
+    )}
     <select>
-        {data && data.length > 0 && data.map((dpr)=>
+        {dpData && dpData.length > 0 && dpData.map((dpr)=>
         <option>{dpr.nom}</option>
         )}
     </select>
